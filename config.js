e4bb0da34cfa6664fb1c38e5bd6b00cc4abc5ccf
valentinoborjacas-tech <valentinoborjacas@gmail.com>
2026-07-20 18:53:22 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+// Pega aquí la URL que te da Apps Script al implementar como Aplicación Web
+// (termina en /exec). La consigues en: Implementar > Nueva implementación.
+const API_URL = 'https://script.google.com/macros/s/AKfycbz-XC3CpBfwaUq614zh-EcvZcL4yo7vMH_iQ1hiRiY9Mvb46ZyN3o3MsKXwiAri3y2Q/exec';
