60beaf083dcaba66e5ec1fd5f2881ea9ced13e4c
valentinoborjacas-tech <valentinoborjacas@gmail.com>
2026-07-21 00:20:54 -0500

    Delete assets directory

--- assets/config.js
+++
@@ -1,3 +0,0 @@
-// Pega aquí la URL que te da Apps Script al implementar como Aplicación Web
-// (termina en /exec). La consigues en: Implementar > Nueva implementación.
-const API_URL = 'PEGA_AQUI_TU_URL_DE_APPS_SCRIPT';
