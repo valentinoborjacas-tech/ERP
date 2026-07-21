cadae968a6e3a2dfae64c9ca56a15000c35ed367
valentinoborjacas-tech <valentinoborjacas@gmail.com>
2026-07-20 19:05:20 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// Pega aquí la URL que te da Apps Script al implementar como Aplicación Web
+// (termina en /exec). La consigues en: Implementar > Nueva implementación.
+const API_URL = 'PEGA_AQUI_TU_URL_DE_APPS_SCRIPT';
