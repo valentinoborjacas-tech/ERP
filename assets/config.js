42648a586eb34169ec12174291ed054aa9ba866e
valentinoborjacas-tech <valentinoborjacas@gmail.com>
2026-07-21 00:43:55 -0500

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,3 +1,3 @@
 // Pega aquí la URL que te da Apps Script al implementar como Aplicación Web
 // (termina en /exec). La consigues en: Implementar > Nueva implementación.
-const API_URL = 'PEGA_AQUI_TU_URL_DE_APPS_SCRIPT';
+const API_URL = 'https://script.google.com/macros/s/AKfycbz-XC3CpBfwaUq614zh-EcvZcL4yo7vMH_iQ1hiRiY9Mvb46ZyN3o3MsKXwiAri3y2Q/exec';
